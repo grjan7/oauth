ff7fc7a11a2816b84e4149eb32138f2cf1851542
Jana Rangasamy <janagr7@gmail.com>
2024-07-08 21:33:46 +0530

    chore: update 404 page

--- packages/auth-server/src/server.js
+++ packages/auth-server/src/server.js
@@ -13,12 +13,12 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', express.static(join(__dirname, '../public')))
-app.use('/admin', adminAPI) // need to implement admin API
-app.use('/auth', authAPI) // need to implement auth API
-app.use('/token', tokenAPI) // need to implement auth API
-app.use('/refreshtoken', tokenAPI) // need to implement auth API
-//app.get('/auth', (req, res) => res.json({ code: 'asfasbasf26xf5dfchudqwe06rRjl8dsd1v-' }))
-//app.get('/token', (req, res) => res.json({ access_token: 'asfasbasf26xf5dfchudqwe06rRjl8dsd1v.sjsdfksffjnfsffdfs.fdsfdsfjhlewqemoeasdksdha5+fsdfvxhcgf', expiresIn: 3600 }))
+// app.use('/admin', adminAPI) // need to implement admin API
+// app.use('/auth', authAPI) // need to implement auth API
+// app.use('/token', tokenAPI) // need to implement auth API
+// app.use('/refreshtoken', tokenAPI) // need to implement auth API
+// app.get('/auth', (req, res) => res.json({ code: 'asfasbasf26xf5dfchudqwe06rRjl8dsd1v-' }))
+// app.get('/token', (req, res) => res.json({ access_token: 'asfasbasf26xf5dfchudqwe06rRjl8dsd1v.sjsdfksffjnfsffdfs.fdsfdsfjhlewqemoeasdksdha5+fsdfvxhcgf', expiresIn: 3600 }))
 
 app.use('*', (req, res) => res.status(404).sendFile(join(__dirname, './404page.html')))
 
