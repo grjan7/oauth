c382e70869d7d717b0eb72badd4807c3ebb9d003
Jana Rangasamy <janagr7@gmail.com>
2024-08-03 23:31:03 +0530

    update index page

--- v2.11/flow/authorization_code/authorization_server/src/index.js
+++ v2.11/flow/authorization_code/authorization_server/src/index.js
@@ -1,3 +1,9 @@
 'use strict'
 
-console.log("hello")
+import app from './server.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+app.listen(process.env.PORT,
+  () => console.log(`OAuth server is listening at ${process.env.PORT}`))
--- v2.11/flow/authorization_code/authorization_server/src/server.js
+++ v2.11/flow/authorization_code/authorization_server/src/server.js
@@ -4,15 +4,23 @@ import express from 'express'
 import cors from 'cors'
 import bodyparser from 'body-parser'
 import morgan from 'morgan'
-import path from 'node:path'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+
 // app.use(cors())
 process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
-app.use('/', express.static(path.join(__dirname, "../public")))
-app.use('*', (req, res) => res.status(400).json({ error: 'Not Found!' }))
+//app.use('/', express.static('public'))
+app.use('/', express.static(join(__dirname, '../public')))
+
+app.use('*', (req, res) => res.status(404).json({ error: 'Not Found!' }))
+
 
 export default app
 
