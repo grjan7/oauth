d893d48429a4aa7c02a24a9e3c69b4743c069aab
Jana Rangasamy <janagr7@gmail.com>
2024-08-04 00:08:28 +0530

    setup db connection

--- v2.11/flow/authorization_code/authorization_server/src/index.js
+++ v2.11/flow/authorization_code/authorization_server/src/index.js
@@ -2,8 +2,22 @@
 
 import app from './server.js'
 import dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
 
-dotenv.config()
+dotenv.config();
+const mongodbURL = process.env.OAUTH_DB_URI;
+const client = new MongoClient(mongodbURL);
 
-app.listen(process.env.PORT,
-  () => console.log(`OAuth server is listening at ${process.env.PORT}`))
+(async () => {
+  try {
+    await client.connect()
+    const accountCollection = client.db("oauth").collection("account")
+    await accountCollection.insertOne({ email: "jdhff@gmail.com" })
+    app.listen(process.env.PORT,
+      () => console.log(`OAuth server is listening at ${process.env.PORT}`))
+  } catch (err) {
+    throw new Error(err)
+  } finally {
+    await client.close()
+  }
+})()
