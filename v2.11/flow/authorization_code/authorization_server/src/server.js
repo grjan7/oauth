b7c2190a559c2a6123c2299a0fb0dfd82b5ffff1
Jana Rangasamy <janagr7@gmail.com>
2024-08-03 23:37:25 +0530

    fix spaces

--- v2.11/flow/authorization_code/authorization_server/src/server.js
+++ v2.11/flow/authorization_code/authorization_server/src/server.js
@@ -11,16 +11,14 @@ import { dirname, join } from 'path'
 const app = express()
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
 
 // app.use(cors())
-process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
-//app.use('/', express.static('public'))
+
 app.use('/', express.static(join(__dirname, '../public')))
-
 app.use('*', (req, res) => res.status(404).json({ error: 'Not Found!' }))
-
 
 export default app
 
