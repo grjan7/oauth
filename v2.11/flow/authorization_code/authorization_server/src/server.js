bcd3ed7a34765da0089e1c596c40ae53af155e8f
Jana Rangasamy <janagr7@gmail.com>
2024-08-03 22:13:08 +0530

    configure app

--- v2.11/flow/authorization_code/authorization_server/src/server.js
+++ v2.11/flow/authorization_code/authorization_server/src/server.js
@@ -1,5 +1,18 @@
 'use strict'
 
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
+import bodyparser from 'body-parser'
+import morgan from 'morgan'
+import path from 'node:path'
+
 const app = express()
+// app.use(cors())
+process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use('/', express.static(path.join(__dirname, "../public")))
+app.use('*', (req, res) => res.status(400).json({ error: 'Not Found!' }))
+
+export default app
 
