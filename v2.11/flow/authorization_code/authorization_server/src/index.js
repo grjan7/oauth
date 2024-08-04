232ac0d3d25dfdbe08cc7a654ddc010536a13846
Jana Rangasamy <janagr7@gmail.com>
2024-08-04 23:10:52 +0530

    update db connection order

--- v2.11/flow/authorization_code/authorization_server/src/index.js
+++ v2.11/flow/authorization_code/authorization_server/src/index.js
@@ -4,20 +4,24 @@ import app from './server.js'
 import dotenv from 'dotenv'
 import { MongoClient } from 'mongodb'
 
-dotenv.config();
-const mongodbURL = process.env.OAUTH_DB_URI;
-const client = new MongoClient(mongodbURL);
+dotenv.config()
 
-(async () => {
+const mongodbURL = process.env.OAUTH_DB_URI
+const client = new MongoClient(mongodbURL)
+
+const init = async () => {
   try {
     await client.connect()
-    const accountCollection = client.db("oauth").collection("account")
-    await accountCollection.insertOne({ email: "jdhff@gmail.com" })
-    app.listen(process.env.PORT,
-      () => console.log(`OAuth server is listening at ${process.env.PORT}`))
+    // const accountCollection = client.db("oauth").collection("account")
+    // await accountCollection.insertOne({ email: "jdhff@gmail.com" })    
   } catch (err) {
-    throw new Error(err)
+    console.log(err.message)
   } finally {
+    app.listen(process.env.PORT, () => {
+      console.log(`OAuth server is listening at ${process.env.PORT}`)
+    })
     await client.close()
   }
-})()
+}
+
+await init()
