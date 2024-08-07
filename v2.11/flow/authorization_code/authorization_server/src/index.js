2b66c73d2d30126e1bf6ee17058bf3cd87ed327b
Jana Rangasamy <janagr7@gmail.com>
2024-08-07 23:36:33 +0530

    update index.js

--- v2.11/flow/authorization_code/authorization_server/src/index.js
+++ v2.11/flow/authorization_code/authorization_server/src/index.js
@@ -1,15 +1,12 @@
 'use strict'
 
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
 import express from 'express'
 import cors from 'cors'
 import bodyparser from 'body-parser'
 import morgan from 'morgan'
 import dotenv from 'dotenv'
-
-// ROUTES
 import users from './api/v1/users.route'
 
 dotenv.config()
@@ -17,22 +14,22 @@ dotenv.config()
 const app = express()
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
-
-// app.use(cors())
+// middlewares
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
+process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
 
-//all top-level routes
+// routes
 app.use('/api/v1/user', users)
-
 app.use('/', express.static(join(__dirname, '../public')))
-app.use('*', (req, res) => res.status(404).json({ error: 'Not Found!' }))
 
+// handle errors 
+app.use('*', (req, res) => res.status(404).json({ error: 'Not Found!' }))
 app.use((err, req, res, next) => {
   res.status(500).send("An unexpected error occured!!")
 })
 
+// set the server up and running !!
 app.listen(process.env.PORT, () => {
   console.log(`OAuth server is listening at ${process.env.PORT}`)
 })
