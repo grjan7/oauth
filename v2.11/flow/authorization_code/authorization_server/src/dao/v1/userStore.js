19767330ceac9661fa40db1497c3077bf5f66d7f
Jana Rangasamy <janagr7@gmail.com>
2024-08-14 22:37:09 +0530

    update user constructor

--- v2.11/flow/authorization_code/authorization_server/src/dao/v1/userStore.js
+++ v2.11/flow/authorization_code/authorization_server/src/dao/v1/userStore.js
@@ -3,7 +3,17 @@
 let userStore
 
 export class User {
-
+  contructor(user) {
+    this.id = user.id
+    this.firstName = user.firstName
+    this.lastName = user.lastName
+    this.dob = user.dob
+    this.hashedPassword = user.hashedPassword
+    this.profileUrl = user.profileUrl
+    this.email = user.email
+    this.phone = user.phone
+    this.preferences = user.preferences
+  }
 }
 
 export class UserStore {
