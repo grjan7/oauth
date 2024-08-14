f819c64a8a9d27c9d5c2796bd6ab188c3b5093da
Jana Rangasamy <janagr7@gmail.com>
2024-08-14 22:33:02 +0530

    update token constructor

--- v2.11/flow/authorization_code/authorization_server/src/dao/v1/tokenStore.js
+++ v2.11/flow/authorization_code/authorization_server/src/dao/v1/tokenStore.js
@@ -3,7 +3,27 @@
 let tokenStore
 
 export class Token {
-
+  constructor(token) {
+    this.id = token.id
+    this.clientID = token.clientID
+    this.userID = token.userID
+    this.redirectUri = token.redirectUri
+    this.scopes = token.scopes
+    this.codeChallange = token.codeChallange
+    this.codeVerifier = token.codeVerifier
+    this.challangeMethod = token.challangeMethod
+    this.state = token.state
+    this.nonce = token.nonce
+    this.authorizationCode = token.authorizationCode
+    this.accessToken = token.accessToken
+    this.idToken = token.idToken
+    this.authorizationCodeGeneratedTime = token.authorizationCodeGeneratedTime
+    this.accessTokenGeneratedTime = token.accessTokenGeneratedTime
+    this.idTokenGeneratedTime = token.idTokenGeneratedTime
+    this.authorizationCodeExpiresAt = token.authorizationCodeExpiresAt
+    this.accessTokenExpiresAt = token.accessTokenExpiresAt
+    this.idTokenExpiresAt = token.idTokenExpiresAt
+  }
 }
 
 export class TokenStore {
