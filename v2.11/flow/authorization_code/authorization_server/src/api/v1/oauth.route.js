575a592f344c25c49c6ac61affce15b6f175a65d
Jana Rangasamy <janagr7@gmail.com>
2024-08-14 13:08:43 +0530

    docs: update flow steps

--- v2.11/flow/authorization_code/authorization_server/src/api/v1/oauth.route.js
+++ v2.11/flow/authorization_code/authorization_server/src/api/v1/oauth.route.js
@@ -5,23 +5,53 @@ import { Router } from 'express'
 const router = new Router()
 
 /**
- * - validate the request parameters (clientID, redirectUri, scopes)
- * - if valid, present the signin page 
+ * - validate the request parameters (clientID, redirectUri, scopes) * 
+ *    - clientStore.findClientById(clientID)
+ *    - oauthCtrl.validateClient({clientID, redirectUri, Scopes}) 
+ * - if valid, present the signin page
+ *    - create token in the token store with
+ *      {
+ *        tokenID
+ *        clientID,
+ *        code_challange,
+ *        challange_method,
+ *        redirect_uri
+ *        state,
+ *        nonce 
+ *      }
+ *    - redirect the user to oauth/sign-in page
+ *    - authenticate the user 
+ *    - on success, update the userID to the token
+ *      {
+ *       tokenID
+ *       clientID,
+ *       userID
+ *      }  
  * - on successful sign-in, prompt the permissions requested by the client app
- * - on successful authorization, create authorization code and store it
- *  to the tokenStore
- * 
+ * - on successful authorization, `POST /permissions`
+ *     - generate authorization_code
+ *     - update the token with authorization_code and scopes authorized
  *  {
  *   clientID,
- *   authorizationCode,
- *   redirectUri,
- *   scopes,
- *   code_challange,
- *   challange_method,
- *   state,
- *   nonce
+ *   authorizationCode
+ *   scopes
  *  }
  * 
+ * - Now, the token will be like
+ * 
+ *  {
+ *    tokenID
+ *    authorizationCode,
+ *    clientID,
+ *    code_challange,
+ *    challange_method
+ *    userID,
+ *    redirectUri,
+ *    state,
+ *    nonce,
+ *    scopes
+ *  }
+ *  on failure, delete the token
  * - finally, post the `authorization_code` to the redirectUri with `state` and `nonce`
  * 
  */
