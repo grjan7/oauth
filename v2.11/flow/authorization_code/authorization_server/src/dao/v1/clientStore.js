74a38ce8464e3f5dc1bcd8a3ae6f76985aac7fcc
Jana Rangasamy <janagr7@gmail.com>
2024-08-14 22:15:04 +0530

    update Client object

--- v2.11/flow/authorization_code/authorization_server/src/dao/v1/clientStore.js
+++ v2.11/flow/authorization_code/authorization_server/src/dao/v1/clientStore.js
@@ -3,7 +3,15 @@
 let clientStore
 
 export class Client {
-
+  contructor(client) {
+    this.userID = client.userID
+    this.name = client.name
+    this.clientID = client.clientID
+    this.clientSecret = client.clientSecret
+    this.redirectUri = client.redirectUri
+    this.scopes = client.scopes
+    this.logoUrl = client.logoUrl
+  }
 }
 
 export class ClientStore {
