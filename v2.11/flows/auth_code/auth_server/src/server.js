e742b78d42c562ee92c835910fde6e4b2ad4544e
Jana Rangasamy <janagr7@gmail.com>
2024-08-21 14:04:01 +0530

    fix: signin routes

--- v2.11/flows/auth_code/auth_server/src/server.js
+++ v2.11/flows/auth_code/auth_server/src/server.js
@@ -18,16 +18,24 @@ app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }))
 process.env.NODE_ENV != 'prod' && app.use(morgan('dev'))
 
+const validateFlow = async (req, res, next) => {
+  if (!req.query.flow) {
+    res.status(401).json({ error: "invalid access" })
+    return
+  }
+  next()
+}
 // routes
 app.use('/account', accounts)
-app.use('/signin', express.static(join(__dirname, '../views/pages/signin')))
-app.use('/', (req, res, next) => res.redirect('/signin'))
+app.use('/signin', validateFlow, express.static(join(__dirname, '../views/pages/signin')))
+app.use('/', (req, res, next) => res.redirect('/signin?flow=default'))
 
 
 // handle errors 
 app.use('*', (req, res) => res.status(404).json({ error: 'Not Found!' }))
 app.use((err, req, res, next) => {
   res.status(500).send("An unexpected error occured!!")
+  next()
 })
 
 export default app
