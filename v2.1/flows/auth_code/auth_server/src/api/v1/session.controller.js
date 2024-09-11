0965bc133856c742dae935728f6db21485a62531
Jana Rangasamy <janagr7@gmail.com>
2024-09-11 22:26:42 +0530

    update validateSession with status messages

--- v2.1/flows/auth_code/auth_server/src/api/v1/session.controller.js
+++ v2.1/flows/auth_code/auth_server/src/api/v1/session.controller.js
@@ -20,23 +20,40 @@ export default class SessionController {
     }
   }
 
+  // this middleware must be called before every protected endpoint handlers
+  // validates the sessionId and redirects to sign in page when
+  // - the cookie does not have `sessionId`
+  // - the sessionId is invalid
+  // - the sessionId does not belong to the user-agent
+
   static async validateSession(req, res, next) {
-    console.log("I am ValidateSession")
     const userAgent = req.headers["user-agent"]
     const { sessionId } = req.cookies
     try {
-      const session = await SessionStore.getSessionBySessionId(sessionId)
-      if (session) {
-        if (session.userAgent == userAgent) {
-          next()
+      if (sessionId) {
+        const session = await SessionStore.getSessionBySessionId(sessionId)
+        if (session) {
+          if (session.userAgent == userAgent) {
+            next()
+          } else {
+            res.status(400).json(
+              { status: `This session does not belong to this useragent ${userAgent}` })
+            //redirect('/signin/flow/default')
+            return
+          }
         } else {
-          res.status(400).redirect('/signin/flow/default')
+          res.status(400).json(
+            { status: `The sessionId ${sessionId} is not valid.` })
+          //redirect('/signin/flow/default')
           return
         }
       } else {
-        res.status(400).redirect('/signin/flow/default')
+        res.status(400).json(
+          { status: `Missing required details` })
+        //redirect('/signin/flow/default')
         return
       }
+
     } catch (e) {
       throw new Error(e)
     }
