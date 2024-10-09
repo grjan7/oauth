20bbcd0d3724ba8b0c416c3dc611e05909644632
Jana Rangasamy <janagr7@gmail.com>
2024-10-09 13:13:18 +0530

    refactor: update side-nav content

--- v2.1/flows/auth_code/auth_server/views/pages/app/js/components/left-panel/left-panel.component.js
+++ v2.1/flows/auth_code/auth_server/views/pages/app/js/components/left-panel/left-panel.component.js
@@ -5,24 +5,19 @@ import { Component } from 'lib/component.class.js'
 const template = `
   <div id="left-panel-component" class="big-panel">  
     <div id="side-nav">
-      <div class="li-0">
-        <p>Settings</p>
-        <div class="li-1">
-          <p>General</p>
-          <p>Security</p>
-        </div>
-      </div>
+      <p>Home</p>
+      <p>Personal Info</p>
+      <p>Account Settings</p>
+      <p>Applications Settings<br>
+        <span>My Applications</span><br>
+        <span>Third-Party Apps</span>
+      </p>
     </div>
   </div>
 `
 
 const style = `
-  li {
-    list-style-type: none;
-  }
   
-  
-
 `
 
 const eventHandlers = {}
